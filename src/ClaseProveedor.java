333f0d5002d680898b8b24e609fa8137eec8a226
marokiyuria0 <huertahenrry127@gmail.com>
2026-03-29 01:24:17 -0300

    proveedor

--- src/ClaseProveedor.java
+++ src/ClaseProveedor.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
+
 public class ClaseProveedor {
     private String rut;
     private String nombre;
     private String numeroTelefono;
+    private ArrayList<ClaseProducto> listaproducto; // lista para agregar a producto
 
     //constructor
     public ClaseProveedor(String rut, String nombre, String numeroTelefono){
         this.rut = rut;
         this.nombre = nombre;
         this.numeroTelefono = numeroTelefono;
+        this.listaproducto = new ArrayList<>();
     }
 
     //getters
@@ -23,6 +27,10 @@ public class ClaseProveedor {
         return numeroTelefono;
     }
 
+    public ArrayList<ClaseProducto> getListaproducto() {
+        return listaproducto;
+    }
+
     //setters
     public void setRut(String rut){
         this.rut = rut;
@@ -34,6 +42,26 @@ public class ClaseProveedor {
 
     public void setNumeroTelefono(String numeroTelefono){
         this.numeroTelefono = numeroTelefono;
+    }
+
+    public void setListaproducto(ArrayList<ClaseProducto> listaproducto) {
+        this.listaproducto = listaproducto;
+    }
+
+
+    //metodos
+    public void registrar(){
+        System.out.println("Proveedor registrado: "+ nombre + " | rut: "+ rut);
+    }
+
+    public void listar(){
+        System.out.println("Proveedor registrado: "+ nombre + " | rut: "+ rut + " | numero de telefono: "+ numeroTelefono);
+        System.out.println("Productos asociados: "+ listaproducto.size());
+    }
+
+    public void asociarProducto(ClaseProducto producto){
+        listaproducto.add(producto);
+        System.out.println("Producto: "+producto.getNombre() +" asociando al proveedor: "+nombre);
     }
 
 }
