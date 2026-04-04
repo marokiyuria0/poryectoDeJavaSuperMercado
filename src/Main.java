404c1c7e247049a3075189a257f5b99f982f1818
marokiyuria0 <huertahenrry127@gmail.com>
2026-04-04 00:59:55 -0300

    creacion de menu principal

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner leer= new Scanner(System.in);
+        int opcion;
+
+        do {
+            system.out.println("SELECCIONE UNA OPCION.\n\n");
+
+            system.out.println("1.AGREGAR PRODUCTO.\n");
+            system.out.println("2.ELIMINAR PRODUCTO.\n");
+            system.out.println("3.VER REGISTROS Y/O VENTAS.\n");
+            system.out.println("4. PROCESAR VENTAS.\n");
+            system.out.println("5. GESTIONAR PRODUCTO.\n");
+            system.out.println("6. CERRAR PROGRAMA.\n");
+            opcion = leer.nextInt();
+
+            switch(opcion){
+                case 1: menuAgregar(); break;
+                case 2: menuEliminar(); break;
+                case 3: menuRegistrosOventas(); break;
+                case 4: menuProcesarVentas(); break;
+                case 5: menuGestionProducto(); break;
+                case 6: System.out.println("CERRANDO PROGRAMA...."); break;
+                default: System.out.println("OPCION NO VALIDA\n\n");
+            }
+
+
+        } while(opcion != 6);
+        System.out.println("PROGRAMA CERRADO....");
+        System.exit(0);
     }
 }
