9790cc4626235fcacd815a8ceb780b36afa0a14a
Gabo <gabrielpehuenche18@gmail.com>
2026-04-14 01:41:33 -0400

    -Actualice la clase de stock

--- src/Stock.java
+++ src/Stock.java
@@ -1,14 +1,13 @@
 public class Stock {
-    protected int cantidadStock;
-    protected int alertaBajoStock; //Revisar (protected) para ver cambios hacia private
-    protected boolean disponibilidad;
+    private int cantidadStock;       //
+    private int alertaBajoStock;     //
+    private boolean disponibilidad;  //
 
     public Stock(int cantidadStock, int alertaBajoStock, boolean disponibilidad) {
         this.cantidadStock = cantidadStock;
         this.alertaBajoStock = alertaBajoStock;
         this.disponibilidad = disponibilidad;
     }
-
 
     public int getCantidadStock() {
         return cantidadStock;
@@ -18,12 +17,13 @@ public class Stock {
         return alertaBajoStock;
     }
 
-    public boolean isDisponibilidad() { // Convención 'is' para booleans
+    public boolean isDisponibilidad() {
         return disponibilidad;
     }
 
     public void setCantidadStock(int cantidadStock) {
         this.cantidadStock = cantidadStock;
+        actualizarDisponibilidad();
     }
 
     public void setAlertaBajoStock(int alertaBajoStock) {
@@ -34,14 +34,9 @@ public class Stock {
         this.disponibilidad = disponibilidad;
     }
 
-
-
-
-
     private void actualizarDisponibilidad() {
-        this.disponibilidad = (this.cantidadStock > 0); // si es verdadero es que esta disponible aunque podria necesitar reposicion
+        this.disponibilidad = (this.cantidadStock > 0);
     }
-
 
     public void aumentarStock(int cantidad) {
         if (cantidad > 0) {
@@ -59,29 +54,21 @@ public class Stock {
         return false;
     }
 
-
-
     public boolean necesitaReposicion() {
-        return this.cantidadStock <= this.alertaBajoStock; // si es true necesita reposicion
+        return this.cantidadStock <= this.alertaBajoStock;
     }
-
 
     public String obtenerEstadoVisual() {
         if (this.cantidadStock == 0) {
-            return "AGOTADO\n";
+            return "AGOTADO";
         } else if (necesitaReposicion()) {
-            return "STOCK CRÍTICO\n";
+            return "STOCK CRÍTICO";
         } else {
-            return "DISPONIBLE\n";
+            return "DISPONIBLE";
         }
     }
 
-    public String cantidadDisponible(){
-        return "LA CANTIDAD QUE QUEDA DEL PRODUCTO ES DE\n" +cantidadStock;
+    public String cantidadDisponible() {
+        return "LA CANTIDAD QUE QUEDA DEL PRODUCTO ES DE " + cantidadStock;
     }
-
-
-
-
-
 }
